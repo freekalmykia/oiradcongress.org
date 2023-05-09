d2b2acea1584b1a98d5c78182da6c6826c9507af
AlSharp <elistaphilly@gmail.com>
2023-05-08 23:17:27 -0400

    change project name

--- config/site.config.js
+++ config/site.config.js
@@ -1,15 +1,15 @@
 export default {
   "metaData": {
-    "title" : "Voices of Captive Nations",
-    "description" : "Voices of Captive Nations tells the world about nations held captive by Moscow",
-    "author" : "Voices of Nations held captive by moscow",
-    "keyword" : "captive nations",
+    "title" : "Oirat-Kalmyk Congress",
+    "description" : "Political organization of Oirat-Kalmyk people",
+    "author" : "Free Kalmykia Foundation, Inc.",
+    "keyword" : "oirats, oirads, kalmyks, Kalmykia, captive nations",
     "ogImage" : "/images/ogimage.jpg"
   },
-  "baseURL" : "https://voicesofcaptivenations.org/",
+  "baseURL" : "https://oiradcongress.org/",
   "logo" : "/images/banter-logo.png",
   "logoText" : "Banter",
   "favicon" : "/images/banter-icon-logo.png",
   "postPerPage" : 6,
-  "copyright" : "© 2022 Voices of Captive Nations. All rights reserved."
+  "copyright" : "© 2023 Oirat-Kalmyk Congress. All rights reserved."
 }
